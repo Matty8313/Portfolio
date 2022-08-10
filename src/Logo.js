654fa535620bd5ebeb9487ec451b62b71592b739
Matty8313 <mateusz03031999@gmail.com>
2022-08-10 13:03:36 +0200

    poprawione-teksty-Logo

--- src/Logo.js
+++ src/Logo.js
@@ -13,12 +13,11 @@ export default function Logo() {
 
         <BackGround/>
         <div id="upper-margin">
-        <Klocreverse id="upper-margin "link="/About"txt="Lorem ipsum dolor sit, amet consectetur adipisicing elit. Hic dignissimos tempore aperiam repellendus fugit, harum doloribus animi maxime praesentium odit ex adipisci illum beatae inventore nobis modi officiis tempora voluptatibus.
-                accusamusminus corrupti, magni porro maiores iusto ullam, pariatur sapiente architecto natus dignissimos nobis id quo ducimus illo, velit repellendus sit?" about="About"
+        <Klocreverse id="upper-margin "link="/About"txt="Until September, I am a student of finance and economics because I have to defend my BA thesis. I am an amateur programmer, because I like it very much that it gives many opportunities to create interesting and useful things for people" about="About"
                 link2="/Skills" txt2="
-                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Hic dignissimos tempore aperiam repellendus fugit, harum doloribus animi maxime praesentium odit ex adipisci illum beatae inventore nobis modi officiis tempora voluptatibus. accusamus minus corrupti, magni porro maiores iusto ullam, pariatur sapiente architecto natus dignissimos nobis id quo ducimus illo, velit repellendus sit? " 
+                I am interested in websites. But in my opinion, wordpress does not offer as much possibilities and efficiency as dedicated websites. That's why I got interested in writing pages from scratch. And so far my favorite are SPA sites, because they give a lot of satisfaction" 
                 about2="Skills"
-                link3="/Projects" txt3="djaskbfkjbdf kjsadbfkdsbf" about3="Projects"
+                link3="/Projects" txt3="I already have several website designs and a mobile application that I use myself. MPlanner, which is used to schedule the week and saves memory in LocalStorage. I ran this web solution through androidStudio and I use it on my phone" about3="Projects"
                 />
         </div>
                 
