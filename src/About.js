6b424c168d7e35ae445b25f223ce9d562ed74efe
Matty8313 <mateusz03031999@gmail.com>
2022-08-10 12:53:57 +0200

    poprawiony tekst

--- src/About.js
+++ src/About.js
@@ -4,11 +4,11 @@ import "./About.scss"
 export default function About(props) {
   return (
     <><div id="body2">
-    <div class="circleAbout" id="CircleMain">Nazywam Się tak i Srak robię to i owo. I dobrze mi z tym. Polubiłem programowanie to se to robie i jest w pyte. Elo elo nie ma czasu na lorem ipsum</div>
-    <div class="circleAbout" id="Circle2">Studiowałem tu i tu i chuja z tego wyszło ale co tam za to jestem</div>
-    <div class="circleAbout" id="Circle3">jeździłem po świecie naprzykład do biedronki i była jazda</div>
-    <div class="circleAbout" id="Circle4">W żabce kupuje fajki, a teraz są w chuj drogie</div>
-    <div class="circleAbout" id="Circle5">W miare wygląda ta strona?</div>
+    <div class="circleAbout" id="CircleMain">My name is Mateusz Płocica and I am a student of finance and accounting. I started my programming adventure 2 years ago</div>
+    <div class="circleAbout" id="Circle2">It started with creating my own website for wordpress because I wanted to create a blog about SEO</div>
+    <div class="circleAbout" id="Circle3">My girlfriend pointed out that I spend more time styling the page than writing articles</div>
+    <div class="circleAbout" id="Circle4">I thought about it and found that writing code was more interesting to me after all</div>
+    <div class="circleAbout" id="Circle5">And so I started to learn programming more seriously</div>
     <div id="BackCircle"><p></p></div>
     <div id="BackCircle-violet"><p></p></div>
     <div id="BackCircle-orange"><p></p></div>
