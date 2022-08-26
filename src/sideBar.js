14a4ec4b73a77cc147e055eb9ecb8fa648f2493f
Matty8313 <mateusz03031999@gmail.com>
2022-08-26 20:21:27 +0200

    master4

--- src/sideBar.js
+++ src/sideBar.js
@@ -26,8 +26,8 @@ export default function SideBar() {
             <ul className="list">
                 <Link to="/Portfolio"><li className="item" onClick={toggleMenu}>Main</li></Link>
                 <Link to="/About"><li className="item" onClick={toggleMenu}>About</li></Link>
-                <Link to="Skills"><li className="item" onClick={toggleMenu}>Skills</li></Link>
-                <Link to="Projects"><li className="item" onClick={toggleMenu}>Projects</li></Link>
+                <Link to="/Skills"><li className="item" onClick={toggleMenu}>Skills</li></Link>
+                <Link to="/Projects"><li className="item" onClick={toggleMenu}>Projects</li></Link>
                 
             </ul>)}
             <button id="btn-x" onClick={toggleMenu}>Menu</button>
