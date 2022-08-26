6cd8f0a3474d3a038eb17f34c53170c231e9f971
Matty8313 <mateusz03031999@gmail.com>
2022-08-26 15:45:21 +0200

    after rework

--- src/About.js
+++ src/About.js
@@ -2,9 +2,38 @@ import React from 'react'
 import "./About.scss"
 
 export default function About(props) {
+    let number = 0
+    
+    
+  setInterval(function(){
+    number = number + 1
+    let text = "My name is Mateusz Płocica and I am a student of finance and accounting. I started my programming adventure 2 years ago"
+    let textCut = text.slice(0,number + 1)
+    let writebox = document.getElementById("writeBox")
+    if(writebox !== null){
+    writebox.innerText = textCut + "_"
+    }
+    else if (number === text.length || writebox === null){
+      clearInterval()
+    }
+
+    
+   });
+   
   return (
-    <><div id="body2">
-    <div class="circleAbout" id="CircleMain">My name is Mateusz Płocica and I am a student of finance and accounting. I started my programming adventure 2 years ago</div>
+    <>
+    <div id="wrap-Box">
+    <div  id="writeBox" ></div>
+    <div  id="writeBox2" ></div>
+    <div  id="writeBox3" >It started with creating my own website for wordpress because I wanted to create a blog about SEO</div>
+    </div>
+    </>
+  )
+}
+
+/* 
+<div id="body2">
+    <div className="circleAbout" id="CircleMain">My name is Mateusz Płocica and I am a student of finance and accounting. I started my programming adventure 2 years ago</div>
     <div class="circleAbout" id="Circle2">It started with creating my own website for wordpress because I wanted to create a blog about SEO</div>
     <div class="circleAbout" id="Circle3">My girlfriend pointed out that I spend more time styling the page than writing articles</div>
     <div class="circleAbout" id="Circle4">I thought about it and found that writing code was more interesting to me after all</div>
@@ -13,6 +42,5 @@ export default function About(props) {
     <div id="BackCircle-violet"><p></p></div>
     <div id="BackCircle-orange"><p></p></div>
     </div>
-    </>
-  )
-}
+
+*/
--- src/Projects.js
+++ src/Projects.js
@@ -1,69 +1,10 @@
-import React, { useState, useRef, useEffect } from 'react'
 import "./Projects.scss"
 
 
 export default function Projects() {
-  let soldiers = 0
-  let  gold2 = 1
-  let house = 0
-  let housePay = 0
-  let risk = 0
-  function houses(){
-    house = house + 1;
-    housePay = house*5;
-    console.log(housePay);
-    console.log(house);
-    gold2 = gold2 - 50;
-    document.getElementById('countH').innerText = 'Your profits: ' + housePay + '$'
-  }
-  function mans(){
-    soldiers = soldiers + 10
-    risk = risk - 1
-    document.getElementById('countS').innerText = 'Risk ' + risk
-  }
-useEffect(() => {
-  const interval = setInterval(() => {
-    gold2 = gold2 + 1 - soldiers + housePay
-    document.getElementById('game-money').innerText = 'money: ' + gold2
-  }, 1000)
-
-  return () => clearInterval(interval);
-}, []);
   return (
     <>
-    <div id='game'>
-      <div class='game-box' id="game-money" >
-      </div>
-      <button class='game-box' id='game-houses' onClick={houses}>
-        houses
-        <div id='cost'>cost:50 gold/once</div>
-        <div id='profits'>profits:5gold/per second</div>
-        <div id='countH'>Your profits:0$</div>  
-      </button>
-      <button class='game-box' id='game-soldiers' onClick={mans}>
-        soldiers
-        <div id='cost'>cost:10 gold/s</div>
-        <div id='profits'>profits: Risk - 1</div>
-        <div id='countS'></div>
-      </button>
-      <button class='game-box' id='game-risk'>
-        Risk
-        <div id='cost'>1</div>
-        <div id='profits'>2</div>
-        <div id='count'>3</div>
-      </button>
-      <div class="item2">domki</div>
-      <div class="item2">żołnierze</div>
-      <div class="item2" id='castle'>zamek</div>
-      <div class="item2">Zagrożenie</div>
-      <div class="item2">Rola</div>
-      <div>
-        <div class="material" id="gold">domki</div>
-        <div class="material" id="gold2">żołnierze</div>
-        <div class="material" id="gold3">zamek</div>
-        <div class="material" id="food">Rola</div>
-      </div>
-    </div>
+
     </>
   )
 }
